55c9de8ef169268804ae686a3d0badf9952082a5
Grigoriy Kosarev <grigoriykosarev@gmail.com>
2017-07-04 17:09:02 +0300

    Если вывод возраста (int) не последний, тогда ошибка

--- src/UserInformation.java
+++ src/UserInformation.java
@@ -14,18 +14,19 @@ public class UserInformation {
         System.out.print("город проживания: ");
         String city = in.nextLine();
 
+        System.out.print("хобби: ");
+        String hobbyUser = in.nextLine();
+
         System.out.print("возраст: ");
         int age = in.nextInt();
-
-        System.out.print("хобби: ");
-        String hobby = in.nextLine();
-
+        
         System.out.println("----------------------------------");
         System.out.println("Вариант 1 (табличный):");
         System.out.println("Имя: "+ name);
         System.out.println("Город: "+ city);
         System.out.println("Возраст: "+ age);
-        System.out.println("Хобби: "+ hobby);
+        System.out.println("Хобби: "+ hobbyUser);
+
 
     }
 }
