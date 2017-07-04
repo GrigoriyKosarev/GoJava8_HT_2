af1bf08c5d5482bd28607a0d23b18d937e5ae220
Grigoriy Kosarev <grigoriykosarev@gmail.com>
2017-07-04 16:51:05 +0300

    ввод данных пользователя через клас Scanner

--- src/UserInformation.java
+++ src/UserInformation.java
@@ -1,7 +1,25 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 /**
  * Created by admin on 04.07.2017.
  */
 public class UserInformation {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Введите имя: ");
+        String name = in.nextLine();
+
+        System.out.print("город проживания: ");
+        String city = in.nextLine();
+
+        System.out.print("возраст: ");
+        int age = in.nextInt();
+
+        System.out.print("хобби: ");
+        String hobby = in.nextLine();
+
+
+    }
 }
